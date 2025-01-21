77a95593e41a2ac1abfb002e831bb0b4ef768897
tblisty <tblisty@gmail.com>
2025-01-21 17:43:34 +0100

    Cache?

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Draguła: Bóg dokonał skrajnej profanacji samego siebie. Doświadczenie sacrum wcale nie jest najważniejsze",
+        "excerpt":"Andrzej Draguła 202501171643 Tako rzecze kard. Sarah: „Bóg znika z naszych społeczeństw, przestaje się liczyć a człowiek zamyka się w samym sobie. Niestety, Kościół wydaje się iść w tym samym kierunku. Skupia się na tym, co horyzontalne, na sprawach społecznych. To nie pomoże w ponownym odkryciu sacrum”. Znam kilku kolegów...","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/2025/01/18/dragula-sarah-profanum.html",
+        "teaser": null
+      },{
+        "title": "Mój pierwszy wpis",
+        "excerpt":"Mój pierwszy wpis   To jest przykładowy tekst w Markdownie.      Pogrubiony tekst: ‘tekst’   Kursywa: ‘tekst’   Link do strony: ‘Google’  ","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/2025/01/18/drugi-wpis.html",
+        "teaser": null
+      },{
+        "title": "The Real Reason Why Analog Recording Is Better",
+        "excerpt":"https://www.youtube.com/watch?v=Zq4BOSaKiYo    ","categories": ["YouTube","links"],
+        "tags": [],
+        "url": "/youtube/links/2025/01/18/why-analog-recording-is-better.html",
+        "teaser": null
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2025/01/18/welcome-to-jekyll.html",
+        "teaser": null
+      }]
